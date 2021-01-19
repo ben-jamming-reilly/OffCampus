a55857a21c2b1973aa41adf48d5407a16c7e026d
ben-jamming-reilly <benreilly0809@gmail.com>
2021-01-19 13:57:07 -0800

    modified:   server.js

--- server.js
+++ server.js
@@ -21,6 +21,5 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-const PORT = 5000;
-//const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log("Server started on " + PORT));
