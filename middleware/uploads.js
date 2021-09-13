e472800312f169b9843722afa4bf01540609ad2b
ben-jamming-reilly <benreilly0809@gmail.com>
2021-09-13 14:15:31 -0700

    modified:   middleware/uploads.js

--- middleware/uploads.js
+++ middleware/uploads.js
@@ -1,13 +1,21 @@
 const multer = require("multer");
 const fs = require("fs");
 
+function getRandomString(length) {
+  let str = "";
+  var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for (let i = 0; i < length; i++)
+    str += characters.charAt(Math.floor(Math.random() * characters.length));
+  return str;
+}
+
 const storageEngine = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, "./uploads/");
   },
   filename: (req, file, cb) => {
     const parts = file.mimetype.split("/");
-    cb(null, String(file.fieldname + "-" + Date.now() + "." + parts[1]));
+    cb(null, String(`${file.fieldname}-${Date.now()}-${getRandomString(3)}.${parts[1]}`));
   },
 });
 
